41cb7e09de2ae10fc94779ff7af55dc4b79348cc
yogaaaaaaa <yogaadhi@icloud.com>
2021-03-17 13:00:57 +0700

    add rectangle and triangle

---
+++ Week 3/Day 13/geometry/rectangle.js
@@ -0,0 +1,7 @@
+const TwoDimention = require("./twoDimention");
+
+class Rectangle extends TwoDimention{
+    constructor(length, width){
+        super("Rectangle");
+    }
+}
--- Week 3/Day 13/geometry/triangle.js
+++ Week 3/Day 13/geometry/triangle.js
@@ -6,4 +6,28 @@ class Rectangle extends twoDimention{
         this.base = base;
         this.height = height;
     }
+
+    //overloading method
+    introduce(who){
+        super.introduce();
+        console.log(`${who}, this is ${this.name} \n`);
+    }
+
+    //overriding
+    calculateArea(){
+    super.calculateArea();
+    let area = this.base * this.height / 2;
+
+    console.log(`This area is ${area} cm2 \n`);
+    }
+
+    calculateCircumference(){
+        super.calculateCircumference();
+        let circumference = 3 * this.base;
+
+        console.log(`this circumference is ${circumference} cm \n`);
+    }
 }
+
+module.exports = Triangle;
+
